b60232ea168726e1a80b73a2cc3e9cfada4c8f36
Hyperverse <jovanigalicia17@gmail.com>
2024-12-03 16:40:00 -0600

    Final del curso Fundamentos de JavaScript

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,3 +1,4 @@
-fetch("https://jsonplaceholder.typicode.com/posts")
+
+fetch("https://jsonplaceholder.typicode.com/posts") // fetch siempre va a ser un get por defecto
   .then((response) => response.json())
   .then((data) => console.log(data));
