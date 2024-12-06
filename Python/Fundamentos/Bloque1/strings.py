3425a24bdfde8dbdc23eb98228aaf2f73953c600
Hyperverse <jovanigalicia17@gmail.com>
2024-12-05 21:20:30 -0600

    Se agregan otros métodos útiles patra strings

--- Python/Fundamentos/Bloque1/strings.py
+++ Python/Fundamentos/Bloque1/strings.py
@@ -55,6 +55,60 @@ print(string5 * 7)
 # La función Len devuelve la longitud de la cadena:
 
 print(len(str4))
+print("-------------------------------------------")
+#Otras funciones interesantes...
+str5 = "I like my coffee just like my metal: Black!"
+str6 = "this message begins with a lowercase letter and the rest of the words do too."
+str7 = "- this message begins and ends with midline -"
+str8 = "aaaaaaaaaaaaa?aaeiiiiiiiiiiiiiiiiiioooo?oooooooouuuuuuuuuuuu"
 
+# .upper convierte todo a mayúsculas
+print(str5.upper())
 
+# .lower convierte todo a minúsculas
+print(str5.lower())
 
+# .count(<arg>) Devuelve el número de apariciones de un caracter (distingue mayúsculas y minúsculas)
+print(str5.count("e"))  
+# igual funciona con substrings
+print(str5.count("coffee")) 
+
+# .capitalize() Regresa el string cambiando la primer letra por mayúscula
+print(str6.capitalize()) 
+
+# .title() devuelve el string cambiando todas sus palabras comenzando con mayúscula
+print(str6.title()) 
+
+# .swapcase() devuelve el string invirtiendo mayúsculas por minúsculas y vice versa
+print(str5.swapcase()) 
+
+# .replace(<arg1>,<arg2>) reemplaza el argumento 1 por el argumento 2 en el string donde se aplica
+print(str5.replace("e","o"))
+
+# .split() separa los elementos de un string en una lista, toma por default el espacio
+newList = str5.split()
+print(newList)
+
+# .strip() elimina al principio y al final el caracter argumentado
+print(str7.strip("-"))
+
+# .lstrip elimina al principio el caracter argumentado 
+print(str7.lstrip("-"))
+
+# .rstrip elimina al final el caracter argumentado 
+print(str7.rstrip("-"))
+
+# .find() devuelve el index de la primera ocurrencia del caracter o string especificado. Devuelve -1 si no se encuentra
+print(str8.find("e"))
+print(str8.find("z"))
+
+# .index() devuelve el índice del caracter o string argumentado
+print(str8.index("?"))
+
+# eval(<expression>) ejecuta una expresión si es una expresión legal para Python
+command = "print('Good bye')"
+eval(command)
+
+# exec(<expression>) similar a eval pero para comandos más complejos
+program = 'a = 5\nb=10\nprint("Sum =", a+b)' # la diagonal invertida y n representan salto de linea
+exec(program)
