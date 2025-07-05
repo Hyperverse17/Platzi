8a982629ef90fc93d21efec55f13767dba5bbe25
Hyperverse <jovanigalicia17@gmail.com>
2025-07-05 13:32:53 -0600

    Reubicacion

--- Python/Fundamentos/04_Funciones/01_function.py
+++
@@ -1,5 +0,0 @@
-
-def greet(name): # Definición de la función
-    print("Hola, " + (str(name)).capitalize()) # Esta función no retorna valores, sólo imprime en pantalla
-
-greet("otelo") # Uso de la función
--- Python/Fundamentos/13_ModulosPaquetes/MyPackage/__init__.py
+++
@@ -1,2 +0,0 @@
-
-# El __init__.py le dice a Python que esa carpeta debe tratarse como un paquete importable.
